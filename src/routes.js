3ae72a2f25aac7d67bf577b737ad2ac5b27ea211
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-02 17:30:07 -0300

    feat: add routes sessions, contacts e users

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,38 @@
 import { Router } from "express";
 import customers from "./app/controllers/CustomersController.js";
+import contacts from "./app/controllers/ContactsController.js"
+import users from "./app/controllers/UserController.js";
+import sessions from "./app/controllers/SessionsController.js";
+import auth from "./app/middlewares/auth.js";
 
 const routes = new Router();
 
+// Sessions
+routes.post("/sessions", sessions.create);
+routes.use(auth)
+
+// Customers
 routes.get("/customers", customers.index);
 routes.get("/customers/:id", customers.show);
 routes.post("/customers/", customers.create);
 routes.put("/customers/:id", customers.update);
 routes.delete("/customers/:id", customers.destroy);
+
+
+// Contacts
+routes.get("/customers/:customerId/contacts", contacts.index);
+routes.get("/customers/:customerId/contacts/:id", contacts.show);
+routes.post("/customers/:customerId/contacts", contacts.create);
+routes.put("/customers/:customerId/contacts/:id", contacts.update);
+routes.delete("/customers/:customerId/contacts/:id", contacts.destroy);
+
+
+// Contacts
+routes.get("/users",  users.index);
+routes.get("/users/:id", users.show);
+routes.post("/users", users.create);
+routes.put("/users/:id", users.update);
+routes.delete("/users/:id", users.destroy);
+
 
 export default routes;
