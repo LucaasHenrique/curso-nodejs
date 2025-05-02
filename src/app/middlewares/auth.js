fde0b72687b46ed2fda4e6f70025397f034cd944
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-02 17:38:04 -0300

    feat: add middleware de validação jwt

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { promisify } from "util";
+
+export default async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: "Token not provided" });
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const decode = jwt.verify(token, process.env.HASH_KEY, {
+            algorithms: ["HS256"],
+        });
+        
+        req.userId = decode.id;
+
+        return next();
+    } catch (err) {
+        return res.status(401).json({error: "Token invalid"});
+    }
+   
+    return next();
+};
