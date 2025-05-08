f4eb6f0a07be99abdfd2c3c1d29d5f71abbbb869
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-08 15:44:10 -0300

    refactor: add variaveis de ambiente database

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();  
+
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'lucas',
-    password: '468321',
-    database: 'cursonode',
+    dialect: 'postgres',    
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
     define: {
         timestamps: true,
         underscored: true,
