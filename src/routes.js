68149aec1c7e07a54adf2333098a06aa93855fab
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-08 16:24:46 -0300

    feat: add rotas de files

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,12 @@ import contacts from "./app/controllers/ContactsController.js"
 import users from "./app/controllers/UserController.js";
 import sessions from "./app/controllers/SessionsController.js";
 import auth from "./app/middlewares/auth.js";
+import multer from "multer";
+import multerConfig from "./config/multer.js";
+import files from "./app/controllers/FilesController.js"
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Sessions
 routes.post("/sessions", sessions.create);
@@ -27,12 +31,13 @@ routes.put("/customers/:customerId/contacts/:id", contacts.update);
 routes.delete("/customers/:customerId/contacts/:id", contacts.destroy);
 
 
-// Contacts
+// Users
 routes.get("/users",  users.index);
 routes.get("/users/:id", users.show);
 routes.post("/users", users.create);
 routes.put("/users/:id", users.update);
 routes.delete("/users/:id", users.destroy);
 
+routes.post("/files", upload.single("file"), files.create);
 
 export default routes;
