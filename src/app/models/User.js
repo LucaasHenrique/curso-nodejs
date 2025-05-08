363d276a2b928a09b548bda64b9f2bf5cc79581e
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-08 15:38:25 -0300

    feat: add relacionamento de users com files

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -22,11 +22,19 @@ class User extends Model {
               user.password_hash = await bcrypt.hash(user.password, 8);
             } 
         });
+
+        return this;
     }
 
     checkPassword(password) {
         return bcrypt.compare(password, this.password_hash);
     }
+
+    static associate(models) {
+		this.belongsTo(models.File, {
+			foreignKey: "file_id",
+		});
+	}
 };
 
 export default User;
